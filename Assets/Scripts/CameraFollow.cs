62a95409c642877f273cb271238a4b39c182f762
Chaofan Zhang <chaofan.zhang@ef.com>
2017-07-06 11:26:16 +0800

    Environment setup, player animation, and camera setup.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+
+	public Transform target;
+	public float smooth = 5f;
+
+	Vector3 offset;
+
+	void Start ()
+	{
+		offset = transform.position - target.position;
+	}
+
+	void FixedUpdate ()
+	{
+		Vector3 newCamPos = offset + target.position;
+		transform.position = Vector3.Lerp (transform.position, newCamPos, smooth * Time.deltaTime);
+	}
+}
